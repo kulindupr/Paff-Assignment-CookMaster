342cad15f10cd3397ae162f5eccc88a65f1e8767
Kulindu Pabasara <kulindupabasara16@gmail.com>
2025-05-10 00:21:30 +0530

    Fix new route error

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
-// import GoogalLogo from './img/glogo.png'
+
 const root = createRoot(document.getElementById("root"));
 
 root.render(
