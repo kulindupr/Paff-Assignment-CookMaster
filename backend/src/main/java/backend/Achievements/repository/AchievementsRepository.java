f4707c12dc2590582454faba7a6d9fb4d954384b
Kulindu Pabasara <kulindupabasara16@gmail.com>
2025-05-09 23:53:27 +0530

    min clean

--- backend/src/main/java/backend/Achievements/repository/AchievementsRepository.java
+++
@@ -1,8 +0,0 @@
-package backend.Achievements.repository;
-
-import backend.Achievements.model.AchievementsModel;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface AchievementsRepository extends MongoRepository<AchievementsModel, String> {
-    void deleteByPostOwnerID(String postOwnerID); // Ensure this method exists
-}
--- backend/src/main/java/backend/PostManagement/repository/PostManagementRepository.java
+++
@@ -1,8 +0,0 @@
-package backend.PostManagement.repository;
-
-import backend.PostManagement.model.PostManagementModel;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface PostManagementRepository extends MongoRepository<PostManagementModel, String> {
-    void deleteByUserID(String userID); // Ensure this method exists
-}
