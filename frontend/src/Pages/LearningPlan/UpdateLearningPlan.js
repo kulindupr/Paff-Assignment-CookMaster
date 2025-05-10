66cf5aa4c7d301dc6ab4b318eb097af3ecf76424
Kulindu Pabasara <kulindupabasara16@gmail.com>
2025-05-10 13:17:32 +0530

    change

--- frontend/src/Pages/LearningPlan/UpdateLearningPlan.js
+++ frontend/src/Pages/LearningPlan/UpdateLearningPlan.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { useParams } from 'react-router-dom';
 import { IoMdAdd } from "react-icons/io";
 import './post.css';
-import './Templates.css';
+import './Templates.css';//template 
 import NavBar from '../../Components/NavBar/NavBar';
 import { FaVideo } from "react-icons/fa";
 import { FaImage } from "react-icons/fa";
