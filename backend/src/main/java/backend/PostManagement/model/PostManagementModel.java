2157a4c7e4b095e56e164192ab880376d484e1d6
Kulindu Pabasara <kulindupabasara16@gmail.com>
2025-05-10 13:06:14 +0530

    new

--- backend/src/main/java/backend/PostManagement/model/PostManagementModel.java
+++ backend/src/main/java/backend/PostManagement/model/PostManagementModel.java
@@ -4,7 +4,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
-import java.util.HashMap;
+import java.util.HashMap;// import java.util.HashMap;
 import java.util.Map;
 import java.util.ArrayList;
 
