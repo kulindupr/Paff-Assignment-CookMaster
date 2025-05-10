9b1b1b6dce83b090ec66ddef596f89676b4aadb4
Asela Wanigabadu <kavindaasela18@gmail.com>
2025-05-10 13:55:09 +0530

    Update background color in AddNewPost component and add new favicon

--- frontend/src/Pages/PostManagement/AddNewPost.js
+++ frontend/src/Pages/PostManagement/AddNewPost.js
@@ -180,7 +180,7 @@ function AddNewPost() {
         padding: '0 15px'
       }}>
         <div className="post-form-container" style={{
-          backgroundColor: 'rgba(255, 255, 255, 0.9)',
+          backgroundColor: 'rgba(198, 244, 240, 0.9)',
           borderRadius: '15px',
           padding: '30px',
           boxShadow: '0 8px 16px rgba(0, 0, 0, 0.3)'
